4b230b4f70eb31e5be476a957ccd7d9d0caa790b
Julio Cesar Bonon <juliocesarbonon@gmail.com>
2021-05-20 09:22:38 -0300

    Update anonDashboard.dart

--- lib/pages/anonDashboard.dart
+++ lib/pages/anonDashboard.dart
@@ -13,43 +13,46 @@ class AnonDashboard extends StatelessWidget {
         child: Column(
           children: [
             SizedBox(height: 40),
-            Container(
-              width: 370,
-              height: 140,
-              decoration: BoxDecoration(
-                color: Colors.green[400],
-                borderRadius: BorderRadius.circular(5),
-              ),
-              child: Padding(
-                padding: const EdgeInsets.all(10.0),
-                child: Column(
-                  children: [
-                    Align(
-                      alignment: Alignment.centerLeft,
-                      child: Icon(Icons.warning),
-                    ),
-                    SizedBox(height: 10),
-                    Text(
-                      "You are currently using Kepp as Anonymous \n Please create an account or login to use all features",
-                      textAlign: TextAlign.center,
-                      style: TextStyle(
-                          fontSize: 15.0, color: Colors.blueGrey[900]),
-                    ),
-                    Align(
-                      alignment: Alignment.bottomLeft,
-                      child: ElevatedButton(
-                        style:
-                            ElevatedButton.styleFrom(primary: Colors.blueGrey),
-                        child: Text(
-                          "Go back",
-                          style: (TextStyle(color: Colors.white)),
-                        ),
-                        onPressed: () {
-                          Navigator.pushNamed(context, '/');
-                        },
+            Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: Container(
+                width: 370,
+                height: 140,
+                decoration: BoxDecoration(
+                  color: Colors.green[400],
+                  borderRadius: BorderRadius.circular(5),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: Column(
+                    children: [
+                      Align(
+                        alignment: Alignment.centerLeft,
+                        child: Icon(Icons.warning),
                       ),
-                    )
-                  ],
+                      SizedBox(height: 10),
+                      Text(
+                        "You are currently using Kepp as Anonymous \n Please create an account or login to use all features",
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                            fontSize: 15.0, color: Colors.blueGrey[900]),
+                      ),
+                      Align(
+                        alignment: Alignment.bottomLeft,
+                        child: ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                              primary: Colors.blueGrey),
+                          child: Text(
+                            "Go back",
+                            style: (TextStyle(color: Colors.white)),
+                          ),
+                          onPressed: () {
+                            Navigator.pushNamed(context, '/');
+                          },
+                        ),
+                      )
+                    ],
+                  ),
                 ),
               ),
             ),
